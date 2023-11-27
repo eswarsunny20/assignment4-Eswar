d69c33312612f3d63a5e4b10b6207ac0d6779fb7
Eswar <keswar466@gmail.com>
2023-11-26 23:12:23 -0500

    Modified index

--- index.js
+++ index.js
@@ -141,21 +141,21 @@ app.get('/api/sales/:invoiceId', async (req, res) => {
 
 
 // Route to show a specific invoice by _id or invoiceID
-app.get('/api/sales/:invoiceId', async (req, res) => {
-    try {
-        const invoiceId = req.params.invoiceId;
-        const invoice = await Sale.findOne({ "Invoice ID": invoiceId });
+// app.get('/api/sales/:invoiceId', async (req, res) => {
+//     try {
+//         const invoiceId = req.params.invoiceId;
+//         const invoice = await Sale.findOne({ "Invoice ID": invoiceId });
 
-        if (!invoice) {
-            return res.status(404).send("Invoice not found");
-        }
+//         if (!invoice) {
+//             return res.status(404).send("Invoice not found");
+//         }
 
-        res.json({ invoice });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Error retrieving invoice data');
-    }
-});
+//         res.json({ invoice });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Error retrieving invoice data');
+//     }
+// });
 
 
 // Route to insert a new invoice
@@ -219,15 +219,15 @@ app.put('/api/sales/:invoiceId', async (req, res) => {
     }
 });
 
-app.get('/api/sales', async (req, res) => {
-    try {
-        const invoices = await Sale.find();
-        res.json({ invoices });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Error retrieving invoice data');
-    }
-});
+// app.get('/api/sales', async (req, res) => {
+//     try {
+//         const invoices = await Sale.find();
+//         res.json({ invoices });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Error retrieving invoice data');
+//     }
+// });
 
 
 
