7f1843244ec582a900fab6cd72cfe28defb91425
Eswar <keswar466@gmail.com>
2023-11-27 00:04:31 -0500

    Modified index

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : "mongodb+srv://eswarkumarsunny:Amma1997@escluster0.omgneyw.mongodb.net/?retryWrites=true&w=majority"
+    url : "mongodb+srv://eswarkumarsunny:Amma1997@escluster0.omgneyw.mongodb.net/sales?retryWrites=true&w=majority"
 };
